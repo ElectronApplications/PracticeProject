457c32c3ace95dff55237e0a4f4d33b011db6bed
stud <stud@istu.int>
2023-06-21 16:30:58 +0800

    Use UTF-32 internally, but normal UTF-8 strings externally

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <codecvt>
+#include <locale>
+
 using namespace std;
 
-const wchar_t alphabet[] = {' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+const char32_t dictionary[] = {' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
                         ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
                         'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-                        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~', L'А', L'Б', L'В', L'Г', L'Д', L'Е', L'Ж', L'З',
-                        L'И', L'Й', L'К', L'Л', L'М', L'Н', L'О', L'П', L'Р', L'С', L'Т', L'У', L'Ф', L'Х', L'Ц', L'Ч', L'Ш', L'Щ', L'Ъ', L'Ы', L'Ь', L'Э',
-                        L'Ю', L'Я', L'а', L'б', L'в', L'г', L'д', L'е', L'ж', L'з', L'и', L'й', L'к', L'л', L'м', L'н', L'о', L'п', L'р', L'с', L'т', L'у',
-                        L'ф', L'х', L'ц', L'ч', L'ш', L'щ', L'ъ', L'ы', L'ь', L'э', L'ю', L'я'};
-map<wchar_t, int> reverse_alphabet;
-const int n = sizeof(alphabet) / sizeof(wchar_t);
+                        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~', U'А', U'Б', U'В', U'Г', U'Д', U'Е', U'Ж', U'З',
+                        U'И', U'Й', U'К', U'Л', U'М', U'Н', U'О', U'П', U'Р', U'С', U'Т', U'У', U'Ф', U'Х', U'Ц', U'Ч', U'Ш', U'Щ', U'Ъ', U'Ы', U'Ь', U'Э',
+                        U'Ю', U'Я', U'а', U'б', U'в', U'г', U'д', U'е', U'ж', U'з', U'и', U'й', U'к', U'л', U'м', U'н', U'о', U'п', U'р', U'с', U'т', U'у',
+                        U'ф', U'х', U'ц', U'ч', U'ш', U'щ', U'ъ', U'ы', U'ь', U'э', U'ю', U'я'};
+map<char32_t, int> reverse_dictionary;
+const int n = sizeof(dictionary) / sizeof(char32_t);
 
 int symbol_encrypt(int symbol, int key) {
     return ((symbol + key) % n + n) % n;
@@ -21,40 +24,73 @@ int symbol_decrypt(int symbol, int key) {
     return ((symbol - key) % n + n) % n;
 }
 
-wstring message_encrypt(wstring message, wstring key) {
-    wstring result = L"";
-    for(int i = 0; i < message.length(); i++) {
-        int symbol = reverse_alphabet[message[i]];
-        int key_symbol = reverse_alphabet[key[i % key.length()]];
-        result += alphabet[symbol_encrypt(symbol, key_symbol)];
+string message_encrypt(string message, string key) {
+    wstring_convert<codecvt_utf8<char32_t>, char32_t> convert;
+
+    u32string message32 = convert.from_bytes(message);
+    u32string key32 = convert.from_bytes(key);
+    u32string result = U"";
+
+    for(int i = 0; i < message32.length(); i++) {
+        int symbol = reverse_dictionary[message32[i]];
+        int key_symbol = reverse_dictionary[key32[i % key32.length()]];
+        result += dictionary[symbol_encrypt(symbol, key_symbol)];
     }
-    return result;
+    return convert.to_bytes(result);
 }
 
-wstring message_decrypt(wstring message, wstring key) {
-    wstring result = L"";
-    for(int i = 0; i < message.length(); i++) {
-        int symbol = reverse_alphabet[message[i]];
-        int key_symbol = reverse_alphabet[key[i % key.length()]];
-        result += alphabet[symbol_decrypt(symbol, key_symbol)];
+string message_decrypt(string message, string key) {
+    wstring_convert<codecvt_utf8<char32_t>, char32_t> convert;
+
+    u32string message32 = convert.from_bytes(message);
+    u32string key32 = convert.from_bytes(key);
+    u32string result = U"";
+    for(int i = 0; i < message32.length(); i++) {
+        int symbol = reverse_dictionary[message32[i]];
+        int key_symbol = reverse_dictionary[key32[i % key32.length()]];
+        result += dictionary[symbol_decrypt(symbol, key_symbol)];
     }
-    return result;
+    return convert.to_bytes(result);
 }
 
 int main() {
-    setlocale(LC_ALL, "Russian");
-
     for(int i = 0; i < n; i++) {
-        reverse_alphabet[alphabet[i]] = i;
+        reverse_dictionary[dictionary[i]] = i;
     }
 
-    wstring message, key;
+    int input = 1;
+    do {
+        cout << "Введите действие:" << endl;
+        cout << "1) Зашифровать строку" << endl;
+        cout << "2) Расшифровать строку" << endl;
+        cout << "3) Выйти из программы" << endl;
+        cin >> input;
+        
+        switch(input) {
+            case 1: {
+                string message, key;
+                cout << "Введите строку, которую нужно зашифровать: " << endl;
+                cin.ignore();
+                getline(cin, message);
+                cout << "Введите ключ, при помощи которого вы хотите зашифровать введенную строку: " << endl;
+                getline(cin, key);
+        
+                cout << "Зашифрованная строка: \"" << message_encrypt(message, key) << "\"" << endl;
+                break;
+            }
+            case 2: {
+                string message, key;
+                cout << "Введите строку, которую нужно расшифровать: " << endl;
+                cin.ignore();
+                getline(cin, message);
+                cout << "Введите ключ, при помощи которого данная строка была зашифрована: " << endl;
+                getline(cin, key);
 
-    getline(wcin, message);
-    getline(wcin, key);
-
-    wcout << message_encrypt(message, key) << endl;
-    wcout << message_decrypt(message_encrypt(message, key), key) << endl;
+                cout << "Расшифрованная строка: \"" << message_decrypt(message, key) << "\"" << endl;
+                break;
+            }
+        }
+    } while(input != 3);
     
     return 0;
 }
